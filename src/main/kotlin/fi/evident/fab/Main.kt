f6793e7fbcadcf15e858b79a3fdf4b20d973872b
Henrik Huttunen <henrik.huttunen@evident.fi>
2017-05-26 22:28:28 +0300

    Refactor

--- src/main/kotlin/fi/evident/fab/Main.kt
+++ src/main/kotlin/fi/evident/fab/Main.kt
@@ -3,7 +3,9 @@ package fi.evident.fab
 import fi.evident.fab.proq2.GlobalPresetParameters
 import fi.evident.fab.proq2.PresetWriter
 import fi.evident.fab.rew.FilterConfigurations
+import fi.evident.fab.rew.FilterPlacement
 import fi.evident.fab.rew.FilterPlacement.*
+import fi.evident.fab.rew.FilterSlope
 import fi.evident.fab.rew.FilterSlope.dB_oct24
 import fi.evident.fab.rew.Parser.parseFilterFile
 import java.io.FileOutputStream
@@ -13,18 +15,24 @@ fun main(args: Array<String>) {
     var outputFile: String? = null
     val configurations = FilterConfigurations()
 
+    fun trimmedArgument(n: Int) = args[n].trim { it <= ' ' }
+
+    fun addConfiguration(slope: FilterSlope, placement: FilterPlacement, parameter: String) {
+        configurations.add(slope, placement, parseFilterFile(parameter))
+    }
+
     for (i in 0 until args.size step 2) {
-        val command = args[i].trim { it <= ' ' }
+        val command = trimmedArgument(i)
 
         if (i+1 >= args.size)
             terminate("Command $command must be followed by a parameter")
 
-        val parameter = args[i+1].trim { it <= ' ' }
+        val parameter = trimmedArgument(i+1)
 
         when (command) {
-            "-stereo" -> configurations.add(dB_oct24, Stereo, parseFilterFile(parameter))
-            "-left" -> configurations.add(dB_oct24, Left, parseFilterFile(parameter))
-            "-right" -> configurations.add(dB_oct24, Right, parseFilterFile(parameter))
+            "-stereo" -> addConfiguration(dB_oct24, Stereo, parameter)
+            "-left" -> addConfiguration(dB_oct24, Left, parameter)
+            "-right" -> addConfiguration(dB_oct24, Right, parameter)
             "-out" -> {
                 if (outputFile != null)
                     terminate("Output cannot be defined multiple times")
@@ -44,7 +52,7 @@ fun main(args: Array<String>) {
         PresetWriter.writePreset(configurations, GlobalPresetParameters(), fos)
     }
 
-    println("done")
+    println("Done")
 }
 
 private fun terminate(reason: String) {
